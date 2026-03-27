b956821f44d20bcbede837618a53b5390ecd1de5
Diggo Silva <rdiggosilva@gmail.com>
2026-03-27 08:20:28 -0300

    Initial configuration

---
+++ DragonBallDex/App/Scenes/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  DragonBallDex
+//
+//  Created by Diggo Silva on 23/03/26.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
