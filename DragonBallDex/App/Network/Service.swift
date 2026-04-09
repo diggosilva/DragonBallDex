617e0c0c878c2268915bb0a71adb6b90b9d5055c
Diggo Silva <rdiggosilva@gmail.com>
2026-04-09 12:44:39 -0300

    Created Service layr to requests

--- DragonBallDex/App/Network/Service.swift
+++ DragonBallDex/App/Network/Service.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+protocol ServiceProtocol {
+    func getCharacters() async throws -> [Char]
+}
+
+final class Service: ServiceProtocol {
+    
+    let baseURL = "https://dragonball-api.com/api"
+    
+    func getCharacters() async throws -> [Char] {
+        guard let url = URL(string: "\(baseURL)/characters") else { throw URLError(.badURL) }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw URLError(.badServerResponse) }
+        
+        let charResponse = try JSONDecoder().decode(CharResponse.self, from: data)
+        
+        return charResponse.items.map({ $0.toDomain() })
+    }
+}
