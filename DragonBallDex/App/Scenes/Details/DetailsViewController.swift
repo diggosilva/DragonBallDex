f4dd877d513f1d9ee66fbb8ef9b143204fbb33a5
Diggo Silva <rdiggosilva@gmail.com>
2026-04-15 18:01:05 -0300

    feat: implement DetailsViewController

--- DragonBallDex/App/Scenes/Details/DetailsViewController.swift
+++ DragonBallDex/App/Scenes/Details/DetailsViewController.swift
@@ -5,4 +5,28 @@
 //  Created by Diggo Silva on 15/04/26.
 //
 
-import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    private let contentView = DetailsView()
+    private let viewModel: DetailsViewModelProtocol
+    
+    init(viewModel: DetailsViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override func loadView() {
+        view = contentView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let char = viewModel.char
+        contentView.configure(char: char)
+        navigationItem.title = char.name
+    }
+}
