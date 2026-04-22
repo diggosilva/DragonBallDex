f11b0f3450914284335a59bb9b27c16221094fa1
Diggo Silva <rdiggosilva@gmail.com>
2026-04-22 09:05:45 -0300

    feat: move showErrorAlert to UIViewController extension

--- DragonBallDex/App/Extensions/UIViewController+Extensions.swift
+++ DragonBallDex/App/Extensions/UIViewController+Extensions.swift
@@ -5,4 +5,14 @@
 //  Created by Diggo Silva on 21/04/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
